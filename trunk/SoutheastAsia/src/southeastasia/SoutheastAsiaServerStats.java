eb307d0a88c39e98e144e63e8a7e7be092872274
enzodayrit11@gmail.com <enzodayrit11@gmail.com@fe89558a-e6ef-4f16-aad3-7fa6acebab8e>
2010-07-28 17:37:28 +0000

    added server stats (server variable handler) and action class (wala pang laman)

---
+++ trunk/SoutheastAsia/src/southeastasia/SoutheastAsiaServerStats.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package southeastasia;
+
+/**
+ *
+ * @author Enzo
+ *
+ * HELOW
+ * this class is for stuff like country variables and chatlogs
+ */
+public class SoutheastAsiaServerStats {
+
+    //to do: put a Dictionary for storing each country's stats (use CountryVariables)
+
+    //to do: chatlog (array siguro of strings)
+
+    //to do: put a Dictionary for storing each country's current actions (use Action)
+
+    public SoutheastAsiaServerStats()
+    {
+
+    }
+
+    //to do: update stats method
+    //to do: get stats method
+
+    //to do: update chatlog method
+    //to do: get chatlog method
+
+    //to do: update actions method
+    //to do: get actions method
+
+}
