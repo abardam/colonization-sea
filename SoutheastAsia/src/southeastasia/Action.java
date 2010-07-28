ba838f5191f080724cc1bd52a99e866dce15a1f3
enzodayrit11@gmail.com <enzodayrit11@gmail.com@fe89558a-e6ef-4f16-aad3-7fa6acebab8e>
2010-07-28 17:37:28 +0000

    added server stats (server variable handler) and action class (wala pang laman)

---
+++ SoutheastAsia/src/southeastasia/Action.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package southeastasia;
+
+/**
+ *
+ * @author Enzo
+ *
+ * this class is for representing actions
+ * idk kung ano laman
+ *
+ * maybe a string for description
+ * tapos idk; stat modifiers?
+ */
+public class Action {
+
+}
