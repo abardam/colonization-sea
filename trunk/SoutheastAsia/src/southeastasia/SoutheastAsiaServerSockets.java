277e3f6d6f230d96314c86280ce02b3dc415773d
akdizon <akdizon@fe89558a-e6ef-4f16-aad3-7fa6acebab8e>
2010-08-02 11:52:13 +0000

    added SoutheastAsiaServerSockets class

---
+++ trunk/SoutheastAsia/src/southeastasia/SoutheastAsiaServerSockets.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package southeastasia;
+
+/**
+ * Coming soon: THREADS
+ * 
+ * @author Albert
+ */
+public class SoutheastAsiaServerSockets {
+
+    int[] players;
+
+    public SoutheastAsiaServerSockets() {
+        players = new int[5];
+    }
+
+    /**
+     * this accepts clients and inputs them into the array
+     * 
+     * to do: add parameters
+     * 
+     */
+    public void acceptSockets() {
+    }
+
+    public int getPlayer() {
+        return 1;
+    }
+}
