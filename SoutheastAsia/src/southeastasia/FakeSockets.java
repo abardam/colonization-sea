e5091a90773e57cd29f4dd6681ee1decc130e6bb
enzodayrit11@gmail.com <enzodayrit11@gmail.com@fe89558a-e6ef-4f16-aad3-7fa6acebab8e>
2010-08-07 16:29:24 +0000

    skeleton of FakeSockets (kelangan pa ng parsing)

---
+++ SoutheastAsia/src/southeastasia/FakeSockets.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package southeastasia;
+import java.util.ArrayList;
+/**
+ *
+ * for simulating sockets
+ * @author Enzo
+ */
+public class FakeSockets {
+    private SoutheastAsiaApp server;
+    private ArrayList<SoutheastAsiaClient> clients;
+    public FakeSockets(SoutheastAsiaApp server)
+    {
+        this.server=server;
+        clients=new ArrayList<SoutheastAsiaClient>();
+
+    }
+
+    public void addClient(SoutheastAsiaClient sac)
+    {
+        clients.add(sac);
+    }
+
+    public void recieveTransmission(String message)
+    {
+        //parse it
+        //call some methods
+
+    }
+}
