a74dcdf4b4032f206743e37b53e16908acd9b2e9
leland.unnerd@gmail.com <leland.unnerd@gmail.com@fe89558a-e6ef-4f16-aad3-7fa6acebab8e>
2011-01-05 15:04:25 +0000

    uh oh. can't see changes. naglagay ako ng new packages. o:

---
+++ trunk/SoutheastAsia/src/southeastasia/networking/SoutheastAsiaServerSockets.java
@@ -0,0 +1,186 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package southeastasia.networking;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import southeastasia.ChatWindow;
+import southeastasia.SoutheastAsiaApp;
+
+/**
+ * Coming soon: THREADS
+ * 
+ * @author Albert
+ */
+public class SoutheastAsiaServerSockets {
+
+    private Socket[] players;
+    private final int port;
+    private final int MAX_PLAYERS;
+    private Accepter accepter;
+    //private ServerSocket[]server;
+    private ServerSocket server;
+    private PrintWriter[] sender;
+    private ChatWindow chat;
+    
+
+    public SoutheastAsiaServerSockets() throws IOException {
+        MAX_PLAYERS = SoutheastAsiaApp.MAX_PLAYERS;
+        players = new Socket[MAX_PLAYERS];
+        //server = new ServerSocket[SoutheastAsiaApp.MAX_PLAYERS];
+        port = 7777;
+        server = new ServerSocket(port);
+        accepter = new Accepter(this);
+        sender = new PrintWriter[MAX_PLAYERS];
+        
+    }
+
+    /**
+     * this accepts clients and inputs them into the array
+     * 
+     * to do: add parameters
+     * 
+     */
+    public void acceptSockets() {
+        accepter.start();
+    }
+
+    public void sendToOne(String message, int player)
+    {
+        sender[player].println(message);
+        //insert string, sender[player] printstream shizz here
+    }
+
+    public void setChat(ChatWindow c)
+    {
+        chat = c;
+    }
+
+    public void sendToAll(String message)
+    {
+        
+        for (int i = 0; i < MAX_PLAYERS; i++)
+        {
+            if (sender[i] != null)  sender[i].println(message);
+        }
+    }
+
+    public void interpret(String order, int player)
+    {
+        //same as above
+        System.out.println("Yo");
+        chat.tempAddMessage("Player " + player + ": " + order);     // Expand tempAddMessage in ChatWindow and this afterwards
+    }
+
+    public void interpret(String order)
+    {
+        System.out.println("FroYo");
+        chat.tempAddMessage(order);     // Expand tempAddMessage in ChatWindow and this afterwards
+    }
+
+    public Socket getPlayer(int i) {
+        return players[i - 1];
+    }
+
+    class Accepter extends Thread
+    {
+        int i;
+        int max;
+        SoutheastAsiaServerSockets ss;
+
+        public Accepter(SoutheastAsiaServerSockets ss) {
+         i = 0;
+         max = SoutheastAsiaApp.MAX_PLAYERS;
+         //max = 1;
+         this.ss = ss;
+        }
+
+        @Override
+        public void run()
+        {
+            while (i < max)
+            {
+
+                    System.out.println("Waiting for player " + (i + 1) + "...");
+
+                    /**
+                     * TODO: Make new threads for the sockets?
+                     * See if it's necessary.
+                     */
+
+                    
+                try
+                {
+                    players[i] = server.accept();
+                    sender[i] = new PrintWriter(players[i].getOutputStream(), true);
+                    PlayRunner g = new PlayRunner(players[i], ss, i);
+                    g.start();
+                    sendToOne("verified",i);
+                }
+                
+                catch (IOException ex)
+                {
+                    Logger.getLogger(SoutheastAsiaServerSockets.class.getName()).log(Level.SEVERE, null, ex);
+                    System.out.println("Jumping Javabeans, Batman, something happened!");
+                }
+
+                i++;
+            }
+            System.out.println("Slots full. Ceasing to accept.");
+        }
+        
+    }
+
+        class PlayRunner extends Thread //doot doot doot. Should I stick this into another class?
+    {
+            Socket socket;
+            SoutheastAsiaServerSockets ss;
+            int i;
+
+            public PlayRunner(Socket socket, SoutheastAsiaServerSockets ss, int i)
+            {
+                this.socket = socket;
+                this.ss = ss;
+                this.i = i;
+            }
+
+        @Override
+            public void run()
+            {
+                try
+                {
+                    
+                    Scanner in = new Scanner(new InputStreamReader(socket.getInputStream()));
+
+                    while(true)
+                    {
+                        String msg = in.nextLine();
+                        if (!(msg.equals("")||msg==null))
+                        {
+                            //interpret(msg); ? Is this how it should be done?
+                            ss.interpret(msg, i);
+                        }
+                        
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Logger.getLogger(SoutheastAsiaServerSockets.class.getName()).log(Level.SEVERE, null, ex);
+                    System.out.println("Yikes! Something happened.");
+                }
+
+
+            }
+
+    }
+    
+}
