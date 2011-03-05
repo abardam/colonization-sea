7e575c8a404612a24f5775bb5f61fe758e7bcdda
enzodayrit11@gmail.com <enzodayrit11@gmail.com@fe89558a-e6ef-4f16-aad3-7fa6acebab8e>
2011-03-05 16:55:13 +0000

    make coordinates easily

--- trunk/SoutheastAsia/src/southeastasia/MapMouseListener.java
+++ trunk/SoutheastAsia/src/southeastasia/MapMouseListener.java
@@ -7,6 +7,10 @@ package southeastasia;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 import southeastasia.game.ClickablePolygon;
 
 /**
@@ -16,17 +20,33 @@ import southeastasia.game.ClickablePolygon;
 public class MapMouseListener implements MouseListener{
 
     private ClickablePolygon[] clickableArea;
+    private BufferedWriter debugWriter;
     public MapMouseListener(ClickablePolygon[] targets)
     {
         clickableArea=targets;
     }
     public void mouseClicked(MouseEvent e) {
         //System.out.println(e.getX()+" "+e.getY());
-        for(int i=0;i<clickableArea.length;i++)
+
+        if(debugWriter==null)
         {
-            if(clickableArea[i].contains(e.getPoint()))
+            for(int i=0;i<clickableArea.length;i++)
             {
-                System.out.println("clicked on "+clickableArea[i].getTargetCountry());
+                if(clickableArea[i].contains(e.getPoint()))
+                {
+                    System.out.println("clicked on "+clickableArea[i].getTargetCountry());
+                }
+            }
+        }
+        else
+        {
+            try
+            {
+                debugWriter.write("<Point>"+e.getX()+","+e.getY()+"</Point>\n");
+            }
+            catch(IOException ioe)
+            {
+                System.out.println(ioe.getMessage());
             }
         }
     }
@@ -35,6 +55,28 @@ public class MapMouseListener implements MouseListener{
     }
 
     public void mouseExited(MouseEvent e) {
+        if(debugWriter!=null)
+        {
+            try
+            {
+                String s = JOptionPane.showInputDialog("Type something, or DONE to save");
+                if(s!=null&&!s.isEmpty())
+                {
+                    debugWriter.write(s+"\n");
+
+                    if(s.equals("DONE"))
+                    {
+                        doneWriting();
+                    }
+                }
+
+
+            }
+            catch(IOException ioe)
+            {
+                System.out.println(ioe.getMessage());
+            }
+        }
     }
 
     public void mousePressed(MouseEvent e) {
@@ -43,4 +85,30 @@ public class MapMouseListener implements MouseListener{
     public void mouseReleased(MouseEvent e) {
     }
 
+    public void setOutfile(String outfile)
+    {
+        try
+        {
+            debugWriter=new BufferedWriter(new FileWriter(outfile));
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void doneWriting()
+    {
+        try
+        {
+            if(debugWriter!=null)
+            {
+                debugWriter.close();
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 }
